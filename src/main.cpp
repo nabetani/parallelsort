990ddd3d4bbdca19f2ebce1fb4efa74f0c4acb95
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-05-06 14:54:00 +0900

    文字列などを追加する

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <parallel/algorithm>
 #include <random>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -11,6 +12,53 @@ struct random_uint32 {
   std::mt19937_64 gen_{9915530857934838174ULL};
   std::uniform_int_distribution<std::uint32_t> dist_{0, ~0U};
   uint32_t operator()() { return dist_(gen_); }
+  static std::string name() { return "random_uint32"; }
+};
+
+template <std::uint32_t kinds> //
+struct few_uint32 {
+  std::mt19937_64 gen_{9915530857934838174ULL};
+  std::uniform_int_distribution<std::uint32_t> dist_{0, kinds};
+  uint32_t operator()() { return dist_(gen_); }
+  static std::string name() {
+    std::stringstream ss;
+    ss << "few_uint32<" << kinds << ">";
+    return ss.str();
+  }
+};
+
+template <size_t len = 1024> //
+struct random_string {
+  std::mt19937_64 gen_{9915530857934838174ULL};
+  std::uniform_int_distribution<std::uint32_t> dist_{'a', 'z'};
+  std::string operator()() {
+    std::string r;
+    r.reserve(len);
+    for (size_t i = 0; i < len; ++i) {
+      r += dist_(gen_);
+    }
+    return r;
+  }
+  static std::string name() {
+    std::stringstream ss;
+    ss << "random_string<" << len << ">";
+    return ss.str();
+  }
+};
+
+template <size_t kinds, size_t len = 1024> //
+struct few_strings {
+  std::mt19937_64 gen_{9915530857934838174ULL};
+  std::uniform_int_distribution<std::uint32_t> dist_{'a', 'a' + kinds - 1};
+  std::string operator()() {
+    char ch = static_cast<char>(dist_(gen_));
+    return std::string(len, ch);
+  }
+  static std::string name() {
+    std::stringstream ss;
+    ss << "few_strings<" << kinds << ", " << len << ">";
+    return ss.str();
+  }
 };
 
 struct gnu_p_sort {
@@ -41,8 +89,8 @@ template <typename value_type, //
           typename sorter,
           typename generator> //
 void test() {
-  size_t min_size = 1 << 8;
-  size_t max_size = 1 << 16;
+  size_t min_size = 1 << 10;
+  size_t max_size = 1 << 20;
   using clock = std::chrono::high_resolution_clock;
   for (size_t size = min_size; size <= max_size; size <<= 1) {
     generator g;
@@ -62,10 +110,11 @@ void test() {
     auto sort_tick = proc([](itor_t a, itor_t b) { sorter::sort(a, b); });
     auto stable_sort_tick =
         proc([](itor_t a, itor_t b) { sorter::stable_sort(a, b); });
-    std::cout                     //
-        << sorter::name() << ", " //
-        << size << ", "           //
-        << sort_tick << ", "      //
+    std::cout                                      //
+        << '"' << sorter::name() << '"' << ", "    //
+        << '"' << generator::name() << '"' << ", " //
+        << size << ", "                            //
+        << sort_tick << ", "                       //
         << stable_sort_tick << std::endl;
   }
 }
@@ -73,5 +122,10 @@ void test() {
 int main() {
   test<uint32_t, gnu_p_sort, random_uint32>();
   test<uint32_t, std_sort, random_uint32>();
-  // test<std::string, random_string>();
+  test<uint32_t, gnu_p_sort, few_uint32<8>>();
+  test<uint32_t, std_sort, few_uint32<8>>();
+  test<std::string, gnu_p_sort, random_string<>>();
+  test<std::string, std_sort, random_string<>>();
+  test<std::string, gnu_p_sort, few_strings<8>>();
+  test<std::string, std_sort, few_strings<8>>();
 }
